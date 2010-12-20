c73641faead4e41597e6a0af097c286a18bc085f
ritsu <ritsu1228@hotmail.com>
2010-12-19 20:30:03 -0600

    Format code. Add to .classpath.

--- src/org/anarxiv/anarxiv.java
+++ src/org/anarxiv/anarxiv.java
@@ -3,10 +3,12 @@ package org.anarxiv;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class anarxiv extends Activity {
+public class anarxiv extends Activity
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
