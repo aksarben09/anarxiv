fc5870a9228cbdf604b868ca223d499331424c9d
ritsu <ritsu1228@hotmail.com>
2010-12-21 03:09:22 -0600

    Added initial tab ui for three views. Added template for basic list item, not used yet.

--- src/org/anarxiv/anarxiv.java
+++ src/org/anarxiv/anarxiv.java
@@ -1,15 +1,61 @@
 package org.anarxiv;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
 
 public class anarxiv extends Activity
 {
+	/* UI components. */
+	private TabHost _tabHost = null;
+	
+	private ListView _uiCategoryList = null;
+	private ListView _uiRecentList = null;
+	private ListView _uiFavoriteList = null;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /* get resource manager. */
+        Resources res = getResources();
+        
+        /* get ui components. */
+        _uiCategoryList = (ListView)findViewById(R.id.categorylist);
+        _uiRecentList = (ListView)findViewById(R.id.recentlist);
+        _uiFavoriteList = (ListView)findViewById(R.id.favlist);
+        
+        /* Tab host setup. */
+        _tabHost = (TabHost)findViewById(R.id.tabhost);
+        _tabHost.setup();
+        
+        /* Category tab. */
+        TabHost.TabSpec tabspec = _tabHost.newTabSpec("category");
+        tabspec.setIndicator(res.getString(R.string.tabstr_Category));
+        tabspec.setContent(R.id.categorylist);
+        _tabHost.addTab(tabspec);
+        
+        /* Recent tab. */
+        tabspec = _tabHost.newTabSpec("recent");
+        tabspec.setIndicator(res.getString(R.string.tabstr_Recent));
+        tabspec.setContent(R.id.recentlist);
+        _tabHost.addTab(tabspec);
+        
+        /* Favorite tab. */
+        tabspec = _tabHost.newTabSpec("fav");
+        tabspec.setIndicator(res.getString(R.string.tabstr_Favorite));
+        tabspec.setContent(R.id.favlist);
+        _tabHost.addTab(tabspec);
+        
+        /* Fill the category list. */
+        String[] catlist = res.getStringArray(R.array.str_Category);
+        _uiCategoryList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, catlist));
+        registerForContextMenu(_uiCategoryList);
     }
 }
