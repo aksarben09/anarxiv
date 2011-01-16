eccfe40f33e6398fc123009896a4f72f464e2ea0
ritsu <ritsu1228@hotmail.com>
2011-01-15 20:28:01 -0600

    Added mainmenu. Added menu swtich for showing different menus for different tabs.

--- src/org/anarxiv/anarxiv.java
+++ src/org/anarxiv/anarxiv.java
@@ -6,13 +6,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.AdapterView;
 
-public class anarxiv extends Activity implements AdapterView.OnItemClickListener
+public class anarxiv extends Activity implements AdapterView.OnItemClickListener, TabHost.OnTabChangeListener
 {
 	/* UI components. */
 	private TabHost _tabHost = null;
@@ -26,6 +29,9 @@ public class anarxiv extends Activity implements AdapterView.OnItemClickListener
 	
 	/** arxiv loader. */
 	private static ArxivLoader _arxivLoader = null;
+	
+	/** id of current tab. */
+	private String _currentTabId = null;
 	
 	/**
 	 * get ArxivLoader instance.
@@ -61,48 +67,49 @@ public class anarxiv extends Activity implements AdapterView.OnItemClickListener
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+    	
+		/* check app root dir. */
+		try
+		{
+			anarxiv.checkAppRootDir();
+		}
+		catch(Exception e)
+		{
+			UiUtils.showToast(this, e.getMessage());
+		}
+    	
+		/* get resource manager. */
+	    Resources res = getResources();
+	    
+	    /* get ui components. */
+	    _uiCategoryList = (ListView)findViewById(R.id.categorylist);
+	    _uiRecentList = (ListView)findViewById(R.id.recentlist);
+	    _uiFavoriteList = (ListView)findViewById(R.id.favlist);
         
-        /* check app root dir. */
-        try
-        {
-        	anarxiv.checkAppRootDir();
-        }
-        catch(Exception e)
-        {
-        	UiUtils.showToast(this, e.getMessage());
-        }
-        
-        /* get resource manager. */
-        Resources res = getResources();
-        
-        /* get ui components. */
-        _uiCategoryList = (ListView)findViewById(R.id.categorylist);
-        _uiRecentList = (ListView)findViewById(R.id.recentlist);
-        _uiFavoriteList = (ListView)findViewById(R.id.favlist);
-        
-        /* event handler. */
+	    /* event handler. */
         _uiCategoryList.setOnItemClickListener(this);
         
         /* Tab host setup. */
         _tabHost = (TabHost)findViewById(R.id.tabhost);
         _tabHost.setup();
+        _tabHost.setOnTabChangedListener(this);
         
         /* Category tab. */
-        TabHost.TabSpec tabspec = _tabHost.newTabSpec("category");
+        TabHost.TabSpec tabspec = _tabHost.newTabSpec(res.getString(R.string.tabid_Category));
         tabspec.setIndicator(res.getString(R.string.tabstr_Category));
         tabspec.setContent(R.id.categorylist);
         _tabHost.addTab(tabspec);
         
         /* Recent tab. */
-        tabspec = _tabHost.newTabSpec("recent");
+        tabspec = _tabHost.newTabSpec(res.getString(R.string.tabid_Recent));
         tabspec.setIndicator(res.getString(R.string.tabstr_Recent));
         tabspec.setContent(R.id.recentlist);
         _tabHost.addTab(tabspec);
         
         /* Favorite tab. */
-        tabspec = _tabHost.newTabSpec("fav");
+        tabspec = _tabHost.newTabSpec(res.getString(R.string.tabid_Favorite));
         tabspec.setIndicator(res.getString(R.string.tabstr_Favorite));
         tabspec.setContent(R.id.favlist);
         _tabHost.addTab(tabspec);
@@ -110,9 +117,11 @@ public class anarxiv extends Activity implements AdapterView.OnItemClickListener
         /* Fill the category list. */
         _uiCategoryList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, /*UrlTable.Category*/_urlTbl.getMainCategoryList()));
         registerForContextMenu(_uiCategoryList);
-    }
+	}
 
-    /** Handler: onItemClick. */
+    /** 
+     * Handler: onItemClick. 
+     */
 	public void onItemClick(AdapterView<?> a, View v, int position, long id) 
 	{
 		/* category clicked. */
@@ -136,5 +145,96 @@ public class anarxiv extends Activity implements AdapterView.OnItemClickListener
 		{
 			
 		}
+	}
+
+	/**
+	 * handler: onTabChanged.
+	 */
+	public void onTabChanged(String tabId) 
+	{
+		// TODO Auto-generated method stub
+		_currentTabId = tabId;
+	}
+	
+	/**
+	 * handler: onPrepareOptionsMenu
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu)
+	{
+		/* tab: category. */
+		if (getResources().getString(R.string.tabid_Category).equals(_currentTabId))
+		{
+			setMenuVisible_Category(menu, true);
+			setMenuVisible_Recent(menu, false);
+			setMenuVisible_Favorite(menu, false);
+		}
+		/* tab: recent. */
+		else if (getResources().getString(R.string.tabid_Recent).equals(_currentTabId))
+		{
+			setMenuVisible_Category(menu, false);
+			setMenuVisible_Recent(menu, true);
+			setMenuVisible_Favorite(menu, false);
+		}
+		/* tab: favorite. */
+		else if (getResources().getString(R.string.tabid_Favorite).equals(_currentTabId))
+		{
+			setMenuVisible_Category(menu, false);
+			setMenuVisible_Recent(menu, false);
+			setMenuVisible_Favorite(menu, true);
+		}
+		
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	/**
+	 * handler: onCreateOptionsMenu.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_main, menu);
+		return true;
+	}
+	
+	/**
+	 * handler: onOptionsItemSelected.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		return super.onOptionsItemSelected(item);
+	}
+	
+	/**
+	 * menu util: tab category.
+	 */
+	private void setMenuVisible_Category(Menu menu, boolean visible)
+	{
+		
+	}
+	
+	/**
+	 * menu util: tab recent.
+	 */
+	private void setMenuVisible_Recent(Menu menu, boolean visible)
+	{
+		MenuItem item = menu.findItem(R.id.mainmenu_recent_category);
+		item.setVisible(visible);
+		item = menu.findItem(R.id.mainmenu_recent_paper);
+		item.setVisible(visible);
+	}
+	
+	/**
+	 * menu util: tab favorite.
+	 */
+	private void setMenuVisible_Favorite(Menu menu, boolean visible)
+	{
+		MenuItem item = menu.findItem(R.id.mainmenu_fav_category);
+		item.setVisible(visible);
+		item = menu.findItem(R.id.mainmenu_fav_paper);
+		item.setVisible(visible);
 	}
 }
